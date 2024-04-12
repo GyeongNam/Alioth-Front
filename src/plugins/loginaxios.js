b2693903274f48d19c9f7b54f59118e5f7557eea
Park Sung Jun <junstin119@gmail.com>
2024-04-12 17:17:52 +0900

    [feat] 디버깅용 log 삭제

--- src/plugins/loginaxios.js
+++ src/plugins/loginaxios.js
@@ -25,20 +25,12 @@ axiosInstance.interceptors.response.use(
   },
   async error => {
     const originalRequest = error.config;
-    
-    //console.log("인터셉터 res에러 :")
-    //console.log(error.response)
 
     if (error.response.status === 401 && !originalRequest._retry) {
       if (error.response.data.message === "토큰을 재발행 합니다.") {
-        console.log("토큰 재발행 시작")
-        console.log(error.response.headers["access-token"])
-        console.log(error.response.headers["fresh-token"])
         localStorage.setItem('accessToken', error.response.headers["access-token"]);
         localStorage.setItem('refreshToken', error.response.headers["fresh-token"]);
         originalRequest.headers.Authorization = `Bearer ${error.response.data.result}`;
-        console.log("재전송 베어러")
-        console.log(originalRequest.headers.Authorization)
         return axiosInstance(originalRequest);
       } else {
         localStorage.removeItem('accessToken');
