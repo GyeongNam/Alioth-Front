5eb5d02a5a8293b73946651f733dcdc4a84adf46
GyeongNam <jo_gn97@naver.com>
2024-04-15 18:01:28 +0900

    [feat] : fullcalendar 적용

--- src/pages/schedule/SchedulePage.vue
+++ src/pages/schedule/SchedulePage.vue
@@ -1,105 +1,139 @@
 <template>
-  <AppSidebar></AppSidebar>
-  <v-main>
-    <AppHeader></AppHeader>
-    <v-container fluid>
-      <div>
-        <h1>일정</h1>
-      </div>
-      <div id="calendar" style="height: 40vw;"></div>
-      <div id="clicked-event"></div>
-      <div>
-        <button @click="changeView('week')">주간</button>
-        <button @click="changeView('month')">월간</button>
-        <button @click="changeView('day')">일간</button>
-      </div>
-    </v-container>
-  </v-main>
+  <div>
+    <AppSidebar></AppSidebar>
+    <v-main>
+      <AppHeader></AppHeader>
+      <v-container fluid>
+        <v-row no-gutters>
+          <v-col>
+            <v-sheet class="pa-2 ma-2">
+            <div style="width: 60vh; height: auto" id="calendarList"></div>
+            </v-sheet>
+          </v-col>
+          <v-col>
+            <v-sheet class="pa-2 ma-2">
+            <FullCalendar style="width: 100vh" :options="calendarOptions"></FullCalendar>
+            </v-sheet>
+          </v-col >
+        </v-row>
+      </v-container>
+    </v-main>
+  </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
 import AppSidebar from "@/layouts/AppSidebar.vue";
 import AppHeader from "@/layouts/AppHeader.vue";
-import Calendar from '@toast-ui/calendar';
-import '@toast-ui/calendar/dist/toastui-calendar.min.css';
-import 'tui-date-picker/dist/tui-date-picker.css';
-import 'tui-time-picker/dist/tui-time-picker.css';
+
+import { Calendar } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list';
+
+let eventGuid = 0
+let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS = [
+  {
+    id: createEventId(),
+    title: 'All-day event',
+    start: todayStr
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: todayStr + 'T12:00:00'
+  }
+]
+
+export function createEventId() {
+  return String(eventGuid++)
+}
 
 export default {
-  components: {AppHeader, AppSidebar},
+  components: { AppHeader, AppSidebar, FullCalendar },
+  mounted() {
+    const calendarEl = document.getElementById('calendarList');
+    const calendar = new Calendar(calendarEl, {
+      plugins: [ listPlugin ],
+      locale: "ko",
+      initialView: 'listDay',
+      views: {
+        listDay: { buttonText: 'list day' },
+        listWeek: { buttonText: 'list week' },
+        listMonth: { buttonText: 'list month' }
+      },
+
+      headerToolbar: {
+        left: 'title',
+        center: '',
+        right: 'listDay,listWeek,listMonth'
+      },
+      events: []
+    });
+    calendar.render();
+  },
   setup() {
-    const calendarInstance = ref(null);
-
-    onMounted(() => {
-      const container = document.getElementById('calendar');
-      const options = {
-        defaultView: 'week',
-        usageStatistics: false,
-        useFormPopup: true,
-        useDetailPopup: true,
-        timezone: {
-          zones: [
-            {
-              timezoneName: 'Asia/Seoul',
-              displayLabel: 'Seoul',
-            },
-            {
-              timezoneName: 'Europe/London',
-              displayLabel: 'London',
-            },
-          ],
-        },
-        calendars: [
-          {
-            id: 'cal1',
-            name: '개인',
-            backgroundColor: '#03bd9e',
-          },
-          {
-            id: 'cal2',
-            name: '직장',
-            backgroundColor: '#00a9ff',
-          },
-        ],
-        template: {
-          time(event) {
-            const { start, end, title } = event;
-            const formatTime = (time) => {
-              const hours = `${time.getHours()}`.padStart(2, '0');
-              const minutes = `${time.getMinutes()}`.padStart(2, '0');
-              return `${hours}:${minutes}`;
-            };
-            return `<span style="color: white;">${formatTime(start)}~${formatTime(end)} ${title}</span>`;
-          },
-          allday(event) {
-            return `<span style="color: gray;">${event.title}</span>`;
-          },
-        },
-        views: ['week', 'month', 'day'],
-      };
-      calendarInstance.value = new Calendar(container, options);
-    });
-
-    onMounted(() => {
-      calendarInstance.value.on('clickEvent', ({ event }) => {
-        const el = document.getElementById('clicked-event');
-        el.innerText = event.title;
-      });
-    });
-
-    const changeView = (view) => {
-      calendarInstance.value.changeView(view); // 선택한 뷰로 전환
-    };
-
+  },
+  data() {
     return {
-      calendarInstance,
-      changeView
-    };
+      isLargeScreen: true,
+      currentEvents: [],
+      calendarOptions: {
+        locale: "ko",
+        plugins: [
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin // needed for dateClick
+        ],
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        initialView: 'dayGridMonth',
+        editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        select: this.handleDateSelect,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents
+      },
+    }
+  },
+  methods: {
+    handleDateSelect(selectInfo) {
+      let title = prompt('Please enter a new title for your event')
+      let calendarApi = selectInfo.view.calendar
+
+      calendarApi.unselect() // clear date selection
+
+      if (title) {
+        calendarApi.addEvent({
+          id: createEventId(),
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay
+        })
+      }
+    },
+    handleEventClick(clickInfo) {
+      if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+        clickInfo.event.remove()
+      }
+    },
+    handleEvents(events) {
+      this.currentEvents = events
+    },
   }
 }
 </script>
 
 <style scoped>
-
+/* 스타일링 */
 </style>
